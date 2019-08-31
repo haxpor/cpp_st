1ad3a094c3438a66f79bad801422bef202fa1fc0
Wasin Thonkaew <haxpor@gmail.com>
2019-08-31 22:27:59 +0800

    Add more complicated case but similar in semantics

--- ConstDefine.cpp
+++ ConstDefine.cpp
@@ -4,10 +4,32 @@
  * Anyway, I feel defining in this way is somewhat confusing, especially when combined with
  * definition of pointer variable type.
  *
- * note: Noticed this kind of style in GLM's template struct of mat. See its function's argument.
+ * note:
+ *  - Noticed this kind of style in GLM's template struct of mat. See its function's argument.
+ *  - when confused, read from right to left
  */
 
 #include <iostream>
+
+static int GlobalVar = 15;
+
+void PrintValue(int const& n)
+{
+    std::cout << n << std::endl;
+}
+
+// most people define it like this for pointer to type id
+void PrintValue(const int* const n)
+{
+    std::cout << *n << std::endl;
+}
+
+// less used, but also possible.
+// Hint: read from right to left for any case.
+void PrintValue2(int const * const n)
+{
+    std::cout << *n << std::endl;
+}
 
 int main()
 {
@@ -15,5 +37,9 @@ int main()
 
     // it should be properly compiled with no warning, and printed as correct value
     std::cout << weird << std::endl;
+
+    PrintValue(GlobalVar);
+    PrintValue(&GlobalVar);
+    PrintValue2(&GlobalVar);
     return 0;
 }
