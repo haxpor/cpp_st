690a549580f82e05a3165e1fd45df6b4eed8b5b4
Wasin Thonkaew <haxpor@gmail.com>
2019-09-04 05:56:54 +0800

    Add StackHeapMemGrow.cpp

---
+++ StackHeapMemGrow.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+class A
+{
+    int a;
+};
+
+class B : public A
+{
+    int b;
+};
+
+class Derived : public A, public B
+{
+    int d;
+};
+
+int main()
+{
+    // stack
+    A a;
+    B b;
+    Derived d;
+
+    // note: low address is below, and high address is above
+    // stack grows down (anyway it's not always guarantee to always be decreasing when printed)
+    std::printf("%7s: %15p\n", "A", std::addressof(a));
+    std::printf("%7s: %15p\n", "B", std::addressof(b));
+    std::printf("%7s: %15p\n", "Derived", std::addressof(d));
+
+    std::printf("\n");
+    
+    // heap
+    A* aPtr = new A();
+    B* bPtr = new B();
+    Derived* dPtr = new Derived();
+
+    // heap grows up (anyway it's not always guarantee to always be decreasing when printed)
+    // thus you should see incresing address location from printed-out text on console on these lines
+    std::printf("%7s: %15p\n", "A", std::addressof(aPtr));
+    std::printf("%7s: %15p\n", "B", std::addressof(bPtr));
+    std::printf("%7s: %15p\n", "Derived", std::addressof(dPtr));
+    return 0;
+}
