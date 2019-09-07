c97d7c0e2c60585453fb9d111d30324c539eb091
Wasin Thonkaew <haxpor@gmail.com>
2019-09-07 19:48:40 +0800

    Add CinRedirection2.cpp
    This is another way of receiving program's arguments via piping mechanism.

---
+++ CinRedirection2.cpp
@@ -0,0 +1,52 @@
+/**
+ * Desmonstrate the use of piping into program.
+ *
+ * program.out < input.txt
+ *
+ * This is another of CinRedirection.cpp.
+ *
+ * Note: There are 2 concepts to accept parameters from external to program
+ *  1. Program's argument via argc, and argv
+ *  2. Piping into the program, accept it via scanf or std::cin
+ *
+ * Use res/sample_input2.txt as input for piping into this program.
+ * Execution usage:
+ *  ./a.out < res/sample_input2.txt
+ */
+#include <cstdio>
+#include <iostream>
+#include <iomanip>
+
+int main(void)
+{
+    {
+        int input1;
+        char input2;
+        long input3;
+        float input4;
+        double input5;
+
+        // c style
+        std::scanf("%d %c %ld %f %lf", &input1, &input2, &input3, &input4, &input5);
+
+        printf("%d\n%c\n%ld\n%.3f\n%.9lf\n", input1, input2, input3, input4, input5);
+    }
+
+    std::cout << "\n";
+
+    {
+        int input1;
+        char input2;
+        long input3;
+        float input4;
+        double input5;
+
+        // c++ style
+        std::cin >> input1 >> input2 >> input3 >> input4 >> input5;
+        std::cout << input1 << "\n" << input2 << "\n" << input3 << "\n";
+        std::cout << std::fixed << std::setprecision(3) << input4 << "\n";
+        std::cout << std::fixed << std::setprecision(9) << input5 << std::endl;
+    }
+    
+    return 0;
+}
