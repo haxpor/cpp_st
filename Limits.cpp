72315cb692e9f591f31a19a83969f218d30471fd
Wasin Thonkaew <haxpor@gmail.com>
2019-09-19 00:03:13 +0800

    Improve Limits.cpp in showing more types, and refactor code

--- Limits.cpp
+++ Limits.cpp
@@ -7,13 +7,26 @@
 #include <limits>
 #include <iomanip>
 
+typedef long long mylong;
+
+template <typename T>
+static void PrintMinMaxValue()
+{
+    T minLong = std::numeric_limits<T>::min();
+    T maxLong = std::numeric_limits<T>::max();
+
+    // typeid() evaluate in run-time, thus use RTTI
+    std::cout << "Minimum value of " << typeid(T).name() << " = " << std::setw(23) << std::right << minLong << "\n"
+              << "Maximum value of " << typeid(T).name() << " = " << std::setw(23) << std::right << maxLong << std::endl;
+}
+
 int main()
 {
-    long minLong = std::numeric_limits<long>::min();
-    long maxLong = std::numeric_limits<long>::max();
-
-    std::cout << "Minimum value of long = " << std::setw(23) << std::right << minLong << "\n"
-              << "Maximum value of long = " << std::setw(23) << std::right << maxLong << std::endl;
+    PrintMinMaxValue<int>();
+    PrintMinMaxValue<long>();
+    PrintMinMaxValue<float>();
+    PrintMinMaxValue<double>();
+    PrintMinMaxValue<mylong>();
 
     return 0;
 }
