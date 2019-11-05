faa8b47147459f02ac9309d24a7c27a469d267f4
Wasin Thonkaew <haxpor@gmail.com>
2019-11-06 01:49:54 +0800

    Fix Single validation with multiple translation unit to be truly as multiple translation unit

---
+++ SingletonValidationMultipleTranslationUnit/Another.cpp
@@ -0,0 +1,21 @@
+#include "Another.h"
+
+void Another::printAddress() const
+{
+    std::cout << "Address of MyManager singleton = " << &(Singleton<MyManager>::GetInstance()) << std::endl;
+}
+
+std::uintptr_t Another::getAddress() const
+{
+    auto& inst = Singleton<MyManager>::GetInstance();
+
+    // the following commented block is just a prove of concept that
+    // pointer variable's address needs not to be the same across multiple translation unit
+    // it's normal variable to hold memory address, but more important thing is address it points to
+    //{
+    //    auto* instPtr = &inst;
+    //    std::cout << "Address of pointer points to singleton = " << &instPtr << std::endl;
+    //}
+
+    return reinterpret_cast<std::uintptr_t>(&inst);
+}
--- SingletonValidationMultipleTranslationUnit/Another.h
+++ SingletonValidationMultipleTranslationUnit/Another.h
@@ -8,27 +8,8 @@
 class Another
 {
 public:
-    // call this method before or after other source file call Singleton<T>::GetInstance()
-    // to let such Singleton exists first or not before depends on test case.
-    void printAddress() const
-    {
-        std::cout << "Address of MyManager singleton = " << &(Singleton<MyManager>::GetInstance()) << std::endl;
-    }
-
-    std::uintptr_t getAddress() const
-    {
-        auto& inst = Singleton<MyManager>::GetInstance();
-
-        // the following commented block is just a prove of concept that
-        // pointer variable's address needs not to be the same across multiple translation unit
-        // it's normal variable to hold memory address, but more important thing is address it points to
-        //{
-        //    auto* instPtr = &inst;
-        //    std::cout << "Address of pointer points to singleton = " << &instPtr << std::endl;
-        //}
-
-        return reinterpret_cast<std::uintptr_t>(&inst);
-    }
+    void printAddress() const;
+    std::uintptr_t getAddress() const;
 };
 
 #endif
