b2d4649a899ffccc9f925a79d983c8fddbf501f7
Wasin Thonkaew <haxpor@gmail.com>
2019-12-20 03:38:20 +0800

    Add more detail about trivially copyable in TriviallyCopyable

--- TriviallyCopyable.cpp
+++ TriviallyCopyable.cpp
@@ -1,6 +1,8 @@
 /**
  * Testbed on checking whether std::vector<scalar-type> is trivially copyable or not via
  * type traits.
+ *
+ * Read more about trivially copyable at Notes section in https://en.cppreference.com/w/cpp/types/is_trivially_copyable
  */
 #include <type_traits>
 #include <iostream>
@@ -10,6 +12,11 @@ std::vector<unsigned int> vec;
 
 int main()
 {
+    unsigned int arr[10];
+    // std::vector is not trivially copyable
     std::cout << std::boolalpha << std::is_trivially_copyable<std::vector<unsigned int>>::value  << std::endl;
+
+    // arr is a pointer, and pointer is trivially copyable
+    std::cout << std::boolalpha << std::is_trivially_copyable<unsigned int[10]>::value << std::endl;
     return 0;
 }
