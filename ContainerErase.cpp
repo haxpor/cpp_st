226543ab2c1b4eab838606877e28da867a8f08be
Wasin Thonkaew <haxpor@gmail.com>
2020-02-20 18:07:47 +0700

    Update ContainerErase.cpp with incorrect usage of erase()

--- ContainerErase.cpp
+++ ContainerErase.cpp
@@ -8,14 +8,27 @@
  */
 #include <unordered_map>
 #include <iostream>
-int main() {
-    std::unordered_multimap<int, int> map {{1,1}, {1,2}, {1,3}, {2,1}};
-    auto it = map.begin();
-    std::advance(it, 1);
-    map.erase(map.begin(), it);     // erase the range from container
-    std::cout << map.size() << '\n';
+#include <cassert>
 
-    map.erase(map.begin(), map.end());  // erase all elements
-    std::cout << map.size() << '\n';
+int main() {
+    {
+        std::unordered_multimap<int, int> map {{1,1}, {1,2}, {1,3}, {2,1}};
+        auto it = map.begin();
+        std::advance(it, 1);
+        map.erase(map.begin(), it);     // erase the range from container
+        assert(map.size() == 3);
+
+        map.erase(map.begin(), map.end());  // erase all elements
+        assert(map.size() == 0);
+    }
+
+    {
+        // This is the incorrect use of erase() which intends to remove all elements resulting from
+        // equal_range(). Result cannot be used in erase() to remove all such elements from the container.
+        //std::unordered_multimap<int, int> map {{1,1}, {1,2}, {2,1}, {1,3}, {2,2}};
+        //auto range = map.equal_range(1);
+        //map.erase(range.first, range.second);
+        //assert(map.size() == 2);
+    }
     return 0;
 }
