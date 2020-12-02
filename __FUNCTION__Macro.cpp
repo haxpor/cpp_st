89ce9184d0f80d0e0e1bb296bc5e1014e19eb62e
Wasin Thonkaew <haxpor@gmail.com>
2020-12-02 21:29:47 +0700

    Add usage of __FUNCTION__ and its variants for all compilers

---
+++ __FUNCTION__Macro.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+struct Widget
+{
+	// more verbose output
+	void foo()
+	{
+#ifdef _MSC_VER
+		std::cout << __FUNCSIG__ << std::endl;
+#elif defined(__GNUC__) || defined(__GNUG__)
+		std::cout << __PRETTY_FUNCTION__ << std::endl;
+#elif __clang
+		std::cout << __FUNCTION__ << std::endl;
+#endif
+	}
+
+	// better printing output
+	void bar()
+	{
+#if defined(__GNUC__) || defined(__GNUG__)
+		std::cout << __PRETTY_FUNCTION__ << std::endl;
+#elif defined(__clang) || defined(_MSC_VER)
+		std::cout << __FUNCTION__ << std::endl;
+#endif
+	}
+};
+
+int main()
+{
+	Widget w;
+	w.bar();
+	return 0;
+}
