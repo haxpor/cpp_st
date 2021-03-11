ac3dc1e85da9753e92c9cc75a0ca034044b199ad
Wasin Thonkaew <haxpor@gmail.com>
2021-03-11 21:43:54 +0100

    Add more info if not to forward declare namespace in UseNamespaceMemberFunctionDefinedAfterUsage.cpp

--- UseNamespaceMemberFunctionDefinedAfterUsage.cpp
+++ UseNamespaceMemberFunctionDefinedAfterUsage.cpp
@@ -2,6 +2,9 @@
 
 // we need to forward declare the function defined inside the namespace before actual use
 // or else compiler will shout out error.
+// 
+// If we don't want to forward declare like this, we need to move the definition of such Detail namespace
+// up here instead.
 namespace Detail { static void Foo(); }
 
 int main()
