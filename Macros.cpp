f961bb591ebaaa2afd9b3d69ff8bfcf4ac757e7c
Wasin Thonkaew <haxpor@gmail.com>
2021-04-14 18:54:01 +0200

    Update with more examples with Macros in Macros.cpp

--- Macros.cpp
+++ Macros.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <cassert>
+#include <cstring>
 
 static int g_int = 1;
 static int Hello = 5;
@@ -14,14 +16,36 @@ static float HelloWorld = 20.0f;
 // ##
 #define M4(x, y) x ## y
 
+// macros accepting name to declare and initialize variable
+template <typename T>
+struct Widget
+{
+	Widget(T val):
+		m_val(val)
+	{}
+
+	T m_val;
+};
+#define DECLARE_TEMPL_VAR(name, type, val) Widget<type> g_##name(val)
+
+// string argument
+#define DECLARE_VAR(moduleName, name, val) int g_##moduleName##_##name = val
+
 int main()
 {
-	std::cout << M1(Hello, World) << std::endl;
-	std::cout << M1(World, Hello) << std::endl;
-	std::cout << M2(World, Hello) << std::endl;
-	std::cout << M3(World, Hello) << std::endl;
+	assert(std::strcmp(M1(Hello, World), "Hello") == 0);
+	assert(std::strcmp(M1(World, Hello), "World") == 0);
+	assert(std::strcmp(M2(World, Hello), "Hello") == 0);
+	assert(std::strcmp(M3(World,Hello), "WorldHello") == 0);
 
-	std::cout << M4(Hello, World) << std::endl;
-	std::cout << M4(11231321, 99999) << std::endl;
+	assert(M4(Hello, World) == HelloWorld);
+	assert(M4(11231321, 99999) == 1123132199999);
+
+	DECLARE_TEMPL_VAR(myTemplVar, int, 10);
+	assert(g_myTemplVar.m_val == 10);
+
+	DECLARE_VAR(TestModuleName, myTest, 2);
+	assert(g_TestModuleName_myTest == 2);
+
 	return 0;
 }
