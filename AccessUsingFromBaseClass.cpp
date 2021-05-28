e2a5a2a51172272907c020c566839a76c1bde135
Wasin Thonkaew <haxpor@gmail.com>
2021-05-28 11:41:05 +0200

    add accessing 'using' from base class without fully qualified name

---
+++ AccessUsingFromBaseClass.cpp
@@ -0,0 +1,28 @@
+/**
+ * To prove that we can access base class's 'using' without a need
+ * to fully qualify the name.
+ *
+ * Constrast this with data member, or function member in which we need to fully
+ * qualify the class name.
+ */
+
+struct Base
+{
+	using pointer_t = void*;
+};
+
+struct Derived : public Base
+{
+	void Foo() const
+	{
+		int val = 0;
+		(pointer_t)&val;			// can access without a need to specify class name like Base::pointer_t
+		(Base::pointer_t)&val;		// also work
+	}
+};
+
+int main()
+{
+	[[maybe_unused]] Derived d;
+	return 0;
+}
