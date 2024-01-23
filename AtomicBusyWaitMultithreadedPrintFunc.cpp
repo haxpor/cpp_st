7ab15f39f0f9b908efedfdcd6c374c6b38e52079
Wasin Thonkaew <haxpor@gmail.com>
2024-01-23 21:11:59 +0700

    add atomic busy-wait approach for printing onto console

---
+++ AtomicBusyWaitMultithreadedPrintFunc.cpp
@@ -0,0 +1,56 @@
+/**
+ * Implementation of atomic busy-wait approach
+ * to synchronize each printing call to console.
+ *
+ * This should be fine if you know ahead that there would be
+ * a very brief time waiting, so it won't busy spin too hard.
+ *
+ * Compile with
+ * g++ -std=c++17 <source-file> -lpthread
+ */
+#include <thread>
+#include <iostream>
+#include <utility>
+#include <atomic>
+
+static std::atomic_flag gPrintLock = ATOMIC_FLAG_INIT;
+
+template<typename... Args>
+static void ts_print(Args&&... args)
+{
+	// spin-lock, that's fine for our usecase
+	while (gPrintLock.test_and_set(std::memory_order_acquire))
+		;
+	
+	((std::cout << std::forward<Args>(args)), ...) << std::endl;
+
+	// release the lock
+	gPrintLock.clear();
+}
+
+struct MyCallable
+{
+	int value;
+
+	MyCallable(): value(0) {}
+	MyCallable(int value_): value(value_) {}
+
+	void operator()(const char* context) const
+	{
+		ts_print("[", context, "] ", value);
+	}
+};
+
+int main()
+{
+	std::thread t1(std::move(MyCallable()), "t1");
+	std::thread t2(std::move(MyCallable()), "t2");
+	std::thread t3(std::move(MyCallable(10)), "t3");
+	std::thread t4(std::move(MyCallable(10)), "t4");
+
+	t1.join();
+	t2.join();
+	t3.join();
+	t4.join();
+	return 0;
+}
